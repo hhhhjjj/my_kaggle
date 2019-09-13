7dcba5a948c8c16acfe21dcb800e02859848516e
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2019-09-13 17:03:24 +0800

    Add files via upload

---
+++ use_numba.py
@@ -0,0 +1,12 @@
+import numba
+
+
+@numba.jit
+# you only to know use the jit to speed up
+def sum2d(arr):
+    M, N = arr.shape
+    result = 0.0
+    for i in range(M):
+        for j in range(N):
+            result += arr[i,j]
+    return result
